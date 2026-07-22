8b094f8635cac7e5d0eea68871ea1351b14c21f9
joshikabi <joshikabi30@gmail.com>
2026-07-22 15:23:00 +0545

    Implement basic chatbot functionality in Chatbot.py
    Implement real bot like chatgpt functionality in chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,10 @@
+Display_massage='l am mini chatbot v1.0'#Hading of app
+print(Display_massage)
+input_massage='say hello for my reply'#Giving massage to user to input hello
+print(input_massage)
+input_massage=input()#storing user input
+Return_massage='Hi,I am here how are you ?'
+print(Return_massage)#massage returning to user
+massage_by_user=input()
+z='You have nice day\n Thanks to use me'#ending of app
+print(z)
